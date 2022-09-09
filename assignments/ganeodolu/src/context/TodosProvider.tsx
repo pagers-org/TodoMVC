49f08ab993a4a6677ea301d260246193a14d3a24
ganeodolu <ganeodolu@gmail.com>
2022-09-10 00:40:59 +0900

    feat: context 사용하여 state, action 전역으로 변경

--- assignments/ganeodolu/src/context/TodosProvider.tsx
+++ assignments/ganeodolu/src/context/TodosProvider.tsx
@@ -1,25 +1,43 @@
-import { createContext, Dispatch, ReactNode, useContext, useMemo, useState } from 'react'
+import { createContext, ReactNode, useContext, useMemo, useState } from 'react'
 import { nanoid } from 'nanoid';
 
 interface Props {
   children: ReactNode
 }
-
+const initialState: Todo[] = [
+  {
+    id: '1',
+    content: '타입 추론',
+    isCompleted: false,
+  },
+  {
+    id: '2',
+    content: '타입 선언',
+    isCompleted: true,
+  },
+  {
+    id: '3',
+    content: '타입 설계',
+    isCompleted: true,
+  }
+].reverse();
 const TodosValueContext = createContext<Todo[]>([]);
-const TodosActionContext = createContext<any>(() => {});
+const TodosActionsContext = createContext<any>(() => { }); // 타입 수정할 것
+
+
 
 const TodosProvider = ({ children }: Props) => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const actions = useMemo(
     () => ({
-      add({ content } : { content: string} ) {
-        if(content.length === 0) return
+      add(content: string) {
+        if (content.length === 0) return
         const newTodo: Todo = {
           id: nanoid(),
           content,
           isCompleted: false,
         }
-        setTodos([newTodo, ...todos])
+        setTodos((prevTodos: Todo[]) => [newTodo, ...prevTodos]);
       },
       toggle(selectedId: string) {
 
@@ -32,12 +50,30 @@ const TodosProvider = ({ children }: Props) => {
   )
 
   return (
-    <TodosActionContext.Provider value={actions}>
+    <TodosActionsContext.Provider value={actions}>
       <TodosValueContext.Provider value={todos}>
         {children}
       </TodosValueContext.Provider>
-    </TodosActionContext.Provider>
+    </TodosActionsContext.Provider>
   )
 }
+
+function useTodosValue() {
+  const value = useContext(TodosValueContext);
+  if (value === undefined) {
+    throw new Error('useTodosValue should be used within TodosProvider');
+  }
+  return value;
+}
+
+function useTodosActions() {
+  const value = useContext(TodosActionsContext);
+  if (value === undefined) {
+    throw new Error('useTodosActions should be used within TodosProvider');
+  }
+  return value;
+}
+
+export { useTodosValue, useTodosActions }
 
 export default TodosProvider
