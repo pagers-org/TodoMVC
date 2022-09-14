234f33bb411aff3cdd1d6b410656db54517304d3
ganeodolu <ganeodolu@gmail.com>
2022-09-14 12:08:16 +0900

    refactor: 변수 분리

--- assignments/ganeodolu/src/components/TodoCounter.tsx
+++ assignments/ganeodolu/src/components/TodoCounter.tsx
@@ -2,10 +2,9 @@ import { useTodosValue } from '../context/TodosProvider';
 
 const TodoCounter = () => {
   const todos = useTodosValue();
+  const uncompletedTodo = todos.filter(todo => !todo.isCompleted);
 
-  return (
-    <p className="todos-count">{`미완료 : ${todos.filter((todo) => !todo.isCompleted).length} 개 / 전체 : ${todos.length} 개`}</p>
-  )
-}
+  return <p className="todos-count">{`미완료 : ${uncompletedTodo.length} 개 / 전체 : ${todos.length} 개`}</p>;
+};
 
-export default TodoCounter
+export default TodoCounter;
