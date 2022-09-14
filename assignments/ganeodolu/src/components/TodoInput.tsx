42fa4e5abd9d3c0bdfa0fb52cec9d1f0686bd25d
ganeodolu <ganeodolu@gmail.com>
2022-09-14 17:36:11 +0900

    refactor: input값 useRef로 변경

--- assignments/ganeodolu/src/components/TodoInput.tsx
+++ assignments/ganeodolu/src/components/TodoInput.tsx
@@ -1,23 +1,33 @@
+import { useRef } from 'react';
 import { useTodosActions } from '../context/TodosProvider';
 
 type HandleSubmitEvent = (e: React.SyntheticEvent<HTMLFormElement>) => void;
 
 const TodoInput = () => {
+  const inputValueRef = useRef<HTMLInputElement>(null);
   const { add } = useTodosActions();
 
   const handleInputSubmit: HandleSubmitEvent = e => {
     e.preventDefault();
-    const todoInputValue = document.forms['todoInputContainer'].elements['todoInput'].value;
-    if (todoInputValue.length === 0) return;
-    add(todoInputValue);
-    document.forms['todoInputContainer'].elements['todoInput'].value = '';
+    if (inputValueRef.current) {
+      const inputValue = inputValueRef.current.value;
+      if (inputValue.length === 0) return;
+      add(inputValue);
+      inputValueRef.current.value = '';
+    }
   };
 
   return (
     <header>
       <h1 className="header-text">Todos</h1>
       <form name="todoInputContainer" className="todo-input-container" onSubmit={handleInputSubmit}>
-        <input type="text" name="todoInput" className="todo-input" placeholder="할일을 입력해주세요" />
+        <input
+          ref={inputValueRef}
+          type="text"
+          name="todoInput"
+          className="todo-input"
+          placeholder="할일을 입력해주세요"
+        />
       </form>
     </header>
   );
